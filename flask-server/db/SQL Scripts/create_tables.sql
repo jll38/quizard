93f3ad71d35a45e0cd5e05e0ad771da9f1cda1ab
Julian Lechner <jll38@njit.edu>
2023-04-03 16:21:56 -0400

    SQL Script edits

--- flask-server/db/SQL Scripts/create_tables.sql
+++ flask-server/db/SQL Scripts/create_tables.sql
@@ -3,5 +3,10 @@ CREATE TABLE IF NOT EXISTS quizzes (
     title TEXT NOT NULL,
     author TEXT NOT NULL,
     questions TEXT NOT NULL,
-    answers TEXT NOT NULL
+    answers TEXT NOT NULL,
+    options TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS existing_ids (
+    id INTEGER PRIMARY KEY
 );
