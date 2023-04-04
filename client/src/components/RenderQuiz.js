b64141042937733901b9dc4631adb2cd1c84d533
Julian Lechner <jll38@njit.edu>
2023-04-04 01:21:15 -0400

    Render questions onto quiz page

--- client/src/components/RenderQuiz.js
+++ client/src/components/RenderQuiz.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import { useState, forwardRef, useImperativeHandle } from 'react';
-import { Box, Text, Image, FormControl, Input, FormLabel,Button, Divider } from '@chakra-ui/react';
+import { Box, Text, Image, FormControl, Input, FormLabel,Button, Divider, Heading } from '@chakra-ui/react';
 import { Form } from 'react-router-dom';
 
-export default function RenderQuiz({id})
+export default function RenderQuiz({data})
 {   
-    
-    
+    const questions = JSON.parse(data[3]);
+    const answers = JSON.parse(data[4]);
+    console.log(questions);
+    console.log(answers);
     return(
         <>
-            <Text>The ID passed from the URL is {id}</Text>
+        <Box maxW='500px'>
+        <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }} borderBottom={'2px'} borderColor='purple.400'>
+            <Text>{data[1]}</Text>
+            <Text fontSize={{ base: 'md', lg: 'lg' }} color={'gray.500'}>By: {data[2]}</Text>
+            </Heading>
+            {questions.map((question, i) => {
+                return(<FormControl name={'q-'+i}>
+                    <Text fontSize={{ base: 'lg', md: 'xl', lg: '2xl' }} fontWeight={'500'}>{question}</Text>
+                </FormControl>);
+            })}
+        </Box>
         </>
     );
 }
--- client/src/pages/Quiz.js
+++ client/src/pages/Quiz.js
@@ -23,29 +23,23 @@ import RenderQuiz from '../components/RenderQuiz';
 function Quiz() {
     const {id} = useParams();
     const [data, setData] = useState();
+    const [isLoading, setLoading] = useState(true);
     useEffect(() => {       
         fetch(`/getQuiz?quizId=${id.toLocaleLowerCase()}`)
   .then(response => response.json())
   .then(data => {
     setData(data.quizData);
     console.log(data.quizData);
+    setLoading(false);
   })
   .catch(error => console.error(error));
       }, [])
     return (
         <>
-            <Stack minH={'80vh'} direction={{ base: 'column', md: 'row' }}>
-                <Flex p={8} flex={12} align={'center'} justify={'center'}>
+            <Stack minH={'80vh'} direction={{ base: 'column', md: 'row' }} w='100%'>
+                <Flex p={8} flex={10} align={'center'} justify={'center'}>
                     <Stack spacing={6} w={'full'} maxW={'lg'}>
-                        <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }} borderBottom={'2px'} borderColor='purple.400'>
-                            <Text
-                                as={'span'}
-                                position={'relative'}>
-                                Take a <Text as={'span'} color={'purple.400'}> Quiz</Text>
-                            </Text>
-                            <br />{' '}
-                            <RenderQuiz id={id.toLowerCase()}></RenderQuiz>
-                        </Heading>
+                            {isLoading ? (<Text>Loading...</Text>) : (<RenderQuiz data={data}></RenderQuiz>)}
                     </Stack>
                 </Flex>
             </Stack>
